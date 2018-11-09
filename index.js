bec34714b2f59c680a90da7938bb911adb1892ce
tofu <athenavladimir@gmail.com>
2018-11-09 17:23:20 +0700

    fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var fs = require("fs");
 server.listen(process.env.PORT || 3001);
 
 console.log("Server is Running :");
+var arrayUser = [];
 
 io.sockets.on('connection',function (socket) { // su kien connect den server
 			// body...
@@ -16,4 +17,21 @@ io.sockets.on('connection',function (socket) { // su kien connect den server
 
 			io.sockets.emit('server-send-data',{noidung : data});
 		});
+
+		socket.on('client-register-user',function(data){
+
+			if(arrayUser.indexOf(data) == -1){
+				// neu tra ve tru 1 tuc la chua ton tai user duoc phep dang ki
+				arrayUser.push(data); // dung push de day ten vao mang
+              console.log("Dang ky thanh cong :"+data); 
+
+			}else {
+			 
+				console.log("Da ton tai user :" +data);
+			}
+			
+		});
+
+
 });
+
